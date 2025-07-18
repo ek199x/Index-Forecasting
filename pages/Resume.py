8b13bddf180d03239477a89d95b72e31887e3d59
ek199x <116383203+ek199x@users.noreply.github.com>
2025-07-18 12:54:40 +0200

    Update Resume.py

--- pages/Resume.py
+++ pages/Resume.py
@@ -109,3 +109,5 @@ with st.expander('Specialized Military Training '):
                 - Airman Leadership School 
                 - Modernization of Enterprise Terminals Sustainment Course
                 ''')
+if st.link_button('Download My Resume', 'https://docs.google.com/u/0/uc?id=1_lGHy33dcjb6uLTjamQlOwwH-FteRjA6&export=download'):
+    
